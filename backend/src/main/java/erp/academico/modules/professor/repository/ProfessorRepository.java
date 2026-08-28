1c43027eebcd13dc4f3169768e3f6749854761e1
beneficios computações <freitas.lucasaf@gmail.com>
2026-08-28 11:55:44 -0300

    atualiza o ProfessorRepository

--- backend/src/main/java/erp/academico/modules/professor/repository/ProfessorRepository.java
+++ backend/src/main/java/erp/academico/modules/professor/repository/ProfessorRepository.java
@@ -5,9 +5,12 @@ import erp.academico.modules.professor.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, UUID> {
+
+    Optional<Professor> findByUsuarioId(UUID usuarioId);
 }
 
