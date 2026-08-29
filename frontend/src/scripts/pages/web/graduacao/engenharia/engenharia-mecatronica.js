a3667a47930e5a41018cf9fccf5f74692289f69c
beneficios computações <freitas.lucasaf@gmail.com>
2026-08-29 01:20:38 -0300

    adiciona a tela de engenharia mecatrônica

---
+++ frontend/src/scripts/pages/web/graduacao/engenharia/engenharia-mecatronica.js
@@ -0,0 +1,6 @@
+import { montarPaginaDeCurso } from "../tecnologia/_curso.js";
+
+// --- MONTA A PÁGINA DO CURSO DE ENGENHARIA MECATRÔNICA ---
+export function montar(raiz) {
+    montarPaginaDeCurso(raiz);
+}
