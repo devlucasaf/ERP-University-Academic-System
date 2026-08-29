89ce91447ced052dac5896d697d269e73d656d88
beneficios computações <freitas.lucasaf@gmail.com>
2026-08-29 01:22:32 -0300

    atualiza e moderniza a tela de ads

---
+++ frontend/src/scripts/pages/web/graduacao/tecnologia/analise-e-desenvolvimento-de-sistemas.js
@@ -0,0 +1,6 @@
+import { montarPaginaDeCurso } from "./_curso.js";
+
+// --- MONTA A PÁGINA DO CURSO DE ANÁLISE E DESENVOLVIMENTO DE SISTEMAS ---
+export function montar(raiz) {
+    montarPaginaDeCurso(raiz);
+}
